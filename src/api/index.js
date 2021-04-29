64be3c6a3142ce8bcec0622afb60a9e7f9df834d
peoray <peoray@yahoo.com>
2021-04-29 13:05:07 +0100

    refactor: API helper

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable camelcase */
-import axios from "axios";
+import axios from 'axios'
 
-const instance = axios.create();
+const instance = axios.create()
 
 // const base = 'https://cors-anywhere.herokuapp.com/http://api.unsplash.com'
-const base = "https://api.unsplash.com";
+const base = 'https://api.unsplash.com'
 
-window.API_HOST = base;
+window.API_HOST = base
 
 // const sessionExpiredEvent = new Event('sessionExpired')
 export default new (class {
@@ -19,7 +19,7 @@ export default new (class {
   //     }
   //   }
 
-  async request(method, url) {
+  async request (method, url) {
     try {
       const res = await instance.request({
         url: `${base}${url}`,
@@ -28,24 +28,24 @@ export default new (class {
         crossdomain: true,
         headers: {
           //   "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          Authorization: "Client-ID vOSaDh8Hmksuy81ulsxLZiVXwXfI5ZAEA5uKeD4GhtQ"
+          'Access-Control-Allow-Origin': '*',
+          Authorization: 'Client-ID vOSaDh8Hmksuy81ulsxLZiVXwXfI5ZAEA5uKeD4GhtQ'
         }
-      });
-      if (res.data.status === "success") {
-        return res.data;
+      })
+      if (res.data.status === 'success') {
+        return res.data
       }
-      return Promise.reject(res.data);
+      return Promise.reject(res.data)
     } catch (e) {
       //   if (e.response && e.response.status === 419) {
       //     window.location = window.location.origin
       //     return Promise.reject(e.response.data)
       //   }
-      return Promise.reject(e);
+      return Promise.reject(e)
     }
   }
 
-  async getPhotos() {
-    return this.request("get", "/photos");
+  async getPhotos () {
+    return this.request('get', '/photos')
   }
-})();
+})()
